bfa170ba221834a2ab5ada34102b895e67f85939
Caarn Heir <caarnheir@gmail.com>
2023-11-08 19:14:06 -0500

    Changed gitsigns config, added "+" as the add sign

--- lua/caarn/plugins/gitsigns.lua
+++ lua/caarn/plugins/gitsigns.lua
@@ -1,5 +1,11 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "+" },
+			},
+		})
+	end,
 }
