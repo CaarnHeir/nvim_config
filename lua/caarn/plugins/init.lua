5b984509875762b22bce90d88bfa293ddb1fde9d
Caarn Heir <caarnheir@gmail.com>
2023-11-17 15:27:20 -0500

    Added vim-visual-multi

--- lua/caarn/plugins/init.lua
+++ lua/caarn/plugins/init.lua
@@ -4,4 +4,6 @@ return {
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 
 	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+
+	"mg979/vim-visual-multi", --multi cursor, multi word selection
 }
