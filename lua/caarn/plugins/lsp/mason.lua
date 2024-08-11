53d8cc1e4eb2e3c43a3799caf6f83aef1a9cf966
Caarn Heir <caarnheir@Caarns-MacBook-Pro.local>
2024-08-11 11:42:39 +0200

    Removed Neorg...tooo slow and I don't use

--- lua/caarn/plugins/lsp/mason.lua
+++ lua/caarn/plugins/lsp/mason.lua
@@ -27,14 +27,13 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"emmet_ls",
-				"zls",
-				"netcoredbg",
+				-- "tsserver",
+				-- "html",
+				-- "cssls",
+				-- "tailwindcss",
+				-- "lua_ls",
+				-- "emmet_ls",
+				-- "zls",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
